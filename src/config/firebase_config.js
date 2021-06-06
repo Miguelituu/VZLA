66e63555984d2b71abe1b440a5d20297dd3204a7
Miguelituu <alex19968910@gmail.com>
2021-06-06 02:33:36 -0400

    Update firebase_config.js

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -1,7 +1,7 @@
 var firebaseConfig = {
     "apiKey": process.env.REACT_APP_apiKey||"AIzaSyDsPufr5Dhusqal0bB8VDD9N6yv9u0Lo1E",
     "authDomain": process.env.REACT_APP_projectId?process.env.REACT_APP_projectId+".firebaseapp.com":"tester-8e38d.firebaseapp.com",
-    "databaseURL": process.env.REACT_APP_projectId?"https://"+process.env.REACT_APP_projectId+(process.env.REACT_APP_databasePrefix?process.env.REACT_APP_databasePrefix:"")+".firebaseio.com":"https://tester-8e38d.firebaseio.com",
+    "databaseURL": process.env.REACT_APP_projectId?"https://"+process.env.REACT_APP_projectId+(process.env.REACT_APP_databasePrefix?process.env.REACT_APP_databasePrefix:"")+"-default-rtdb.firebaseio.com":"https://tester-8e38d.firebaseio.com",
     "projectId": process.env.REACT_APP_projectId?process.env.REACT_APP_projectId:"tester-8e38d",
     "storageBucket": process.env.REACT_APP_projectId?process.env.REACT_APP_projectId+".appspot.com":"tester-8e38d.appspot.com",
     "messagingSenderId": "490493205074",
